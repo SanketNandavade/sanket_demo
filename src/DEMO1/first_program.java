11d0b5fce9160e0099701ef012195c3c377085ba
hp <hp@DESKTOP-OB14938>
2022-05-01 15:32:54 +0530

    first program added

---
+++ src/DEMO1/first_program.java
@@ -0,0 +1,17 @@
+package DEMO1;
+
+public class first_program {
+	
+	public static void main(String[] args) {
+		
+	
+	// TODO Auto-generated method stub
+	
+	System.out.println("ECTC");
+	System.out.println("Jan2022");
+	System.out.println("Batch");
+	
+	
+	}
+}
+	
